9fc2ef2ae5493958bc822605a6322c4b67a76cce
Melv Kim <melvkim@gmail.com>
2013-05-09 00:56:35 -0700

    Improved: install.sh; Added: paths for multiple versions of IntelliJ preferences folder.

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-for I in ~/Library/Preferences/IntelliJIdea*; do
+# find path for IntelliJ preferences directory (can be multiple)
+I=$(find ~/Library/Preferences -type d -name IntelliJIdea* -o -type d -name IdeaIC*)
+
+# execute `curl` to save keymap
+for temp in $I; do
 	X="$I/keymaps/Eclipse on Mac.xml"
 	echo Installing as $X
 	curl "https://raw.github.com/thatha/IntelliJ-IDEA-Eclipse-on-Mac-Keymap/master/Eclipse%20on%20Mac.xml" > "$X"
